cbecfc25fd7d2513a2b443d62742ee87a4b1c7ff
Renan.Belarmino <69680055+RenanBelarmino@users.noreply.github.com>
2023-06-19 15:45:39 -0300

    Update owaspzap.groovy

--- vars/owaspzap.groovy
+++ vars/owaspzap.groovy
@@ -1,4 +1,5 @@
-def call(String url) {
-    sh 'docker pull owasp/zap2docker-stable --privileged --name jenkins jenkins'
-    sh 'docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-stable zap-full-scan.py -t ${url} -g gen.conf -r testreport.html --privileged --name jenkins jenkins'
+def call() {
+        sh """
+          docker run --rm hello_there
+        """
 }
