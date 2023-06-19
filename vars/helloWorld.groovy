12dda2b4ff67c1c0fa75a395e6394ed7b45d3808
Renan.Belarmino <69680055+RenanBelarmino@users.noreply.github.com>
2023-06-19 11:52:32 -0300

    Update helloWorld.groovy

--- vars/helloWorld.groovy
+++ vars/helloWorld.groovy
@@ -1,3 +1,8 @@
-def call(Map config = [:]) {
-    sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
+def call() {
+    sh "echo Hello"
 }
+
+
+//def call(Map config = [:]) {
+//    sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
+//}
